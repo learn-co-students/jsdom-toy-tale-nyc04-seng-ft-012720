f54095a2c414f8cbdfeb6aa2d1e9156bba91ebc7
Jeremy Perry <jeremy.m.perry@gmail.com>
2020-03-13 13:26:10 -0400

    master commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 let addToy = false;
+let collection = document.querySelector('#toy-collection')
 
 document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyForm = document.querySelector(".container");
-  addBtn.addEventListener("click", () => {
-    // hide & seek with the form
-    addToy = !addToy;
-    if (addToy) {
-      toyForm.style.display = "block";
-    } else {
-      toyForm.style.display = "none";
+    /*Grab all the data from our backend using fetch. Throw it in a function
+    might use it later*/
+    
+    function toyList(){
+    fetch('http://localhost:3000/toys')
+    // .then(res => res.json())
+    console.log("done")
     }
-  });
-});
+})
+
