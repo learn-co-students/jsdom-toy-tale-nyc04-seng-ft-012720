47bf4d3de37ce19230f9af1d68b46c2b3fb5a20e
dagpan <panagiotisntagkinis@gmail.com>
2020-03-13 14:19:57 -0400

    For Jeremy

--- src/index.js
+++ src/index.js
@@ -1,15 +1,68 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyForm = document.querySelector(".container");
+
+
+  const toyList = document.querySelector("#toy-collection")
+  const addBtn = document.querySelector("#new-toy-btn")
+  const toyForm = document.querySelector(".container")
+  const newToyForm = document.querySelector(".add-toy-form")
+  fetch("http://localhost:3000/toys")
+       .then(response => response.json())
+       .then(actualToyData => {
+             renderAllToys(actualToyData)
+       })
+  function renderAllToys(toys) {
+           toys.forEach(renderOneToy)
+  }
+
+  newToyForm.addEventListener("submit", handleFormSubmit)
+  
   addBtn.addEventListener("click", () => {
-    // hide & seek with the form
-    addToy = !addToy;
+    // hide & seek with the form  
+    addToy = !addToy
     if (addToy) {
-      toyForm.style.display = "block";
+      toyForm.style.display = "block"
     } else {
-      toyForm.style.display = "none";
+      toyForm.style.display = "none"
     }
-  });
-});
+  })
+    
+  function handleFormSubmit(event) {
+      event.preventDefault()
+      const newToy = {
+            name: event.target["name"].value,
+            image: event.target["image"].value,
+            likes: 0
+            }
+      debugger
+      fetch("http://localhost:3000/toys", {
+             method: "POST",
+             headers: {
+                       "Content-Type": "application/json",
+                       "Accept": "application/json" 
+                      },
+             body: JSON.stringify(newToy)
+             })
+            .then(response => response.json())
+            .then(actualNewToy => {
+                 console.log(actualNewToy)
+                 debugger
+                 renderOneToy(actualNewToy)
+            })
+  }
+  function renderOneToy(toyObj) {
+      const cardDiv = document.createElement('div')
+      cardDiv.className = "card"
+      cardDiv.dataset.id = toyObj.id
+      cardDiv.innerHTML = `
+      <h2 class="name">${toyObj.name}</h2>
+      <img class="toy-avatar" src="${toyObj.image}" alt="${toyObj.name}">
+      <p class="likes"><strong>${toyObj.likes} Likes</strong></p>
+      <button data-action="like" class="like-btn">❤️</button>
+    `
+    toyList.append(cardDiv)
+  }
+
+})
+
