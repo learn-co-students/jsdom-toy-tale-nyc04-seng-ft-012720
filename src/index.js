00126e4247bba0aa7ac2f9202edbaf7a2b7bf7d5
Jeremy Perry <jeremy.m.perry@gmail.com>
2020-03-13 14:27:28 -0400

    Second Commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,68 @@
 let addToy = false;
-let collection = document.querySelector('#toy-collection')
 
 document.addEventListener("DOMContentLoaded", () => {
-    /*Grab all the data from our backend using fetch. Throw it in a function
-    might use it later*/
-    
-    function toyList(){
-    fetch('http://localhost:3000/toys')
-    // .then(res => res.json())
-    console.log("done")
+
+
+  const toyList = document.querySelector("#toy-collection")
+  const addBtn = document.querySelector("#new-toy-btn")
+  const toyForm = document.querySelector(".container")
+  const newToyForm = document.querySelector(".add-toy-form")
+  fetch("http://localhost:3000/toys")
+       .then(response => response.json())
+       .then(actualToyData => {
+             renderAllToys(actualToyData)
+       })
+  function renderAllToys(toys) {
+           toys.forEach(renderOneToy)
+  }
+
+  newToyForm.addEventListener("submit", handleFormSubmit)
+  
+  addBtn.addEventListener("click", () => {
+    // hide & seek with the form  
+    addToy = !addToy
+    if (addToy) {
+      toyForm.style.display = "block"
+    } else {
+      toyForm.style.display = "none"
     }
+  })
+    
+  function handleFormSubmit(event) {
+      event.preventDefault()
+      const newToy = {
+            name: event.target["name"].value,
+            image: event.target["image"].value,
+            likes: 0
+            }
+    //   debugger
+      fetch("http://localhost:3000/toys", {
+             method: "POST",
+             headers: {
+                       "Content-Type": "application/json",
+                       "Accept": "application/json" 
+                      },
+             body: JSON.stringify(newToy)
+             })
+            .then(response => response.json())
+            .then(actualNewToy => {
+                 console.log(actualNewToy)
+                 debugger
+                 renderOneToy(actualNewToy)
+            })
+  }
+  function renderOneToy(toyObj) {
+      const cardDiv = document.createElement('div')
+      cardDiv.className = "card"
+      cardDiv.dataset.id = toyObj.id
+      cardDiv.innerHTML = `
+      <h2 class="name">${toyObj.name}</h2>
+      <img class="toy-avatar" src="${toyObj.image}" alt="${toyObj.name}">
+      <p class="likes"><strong>${toyObj.likes} Likes</strong></p>
+      <button data-action="like" class="like-btn">❤️</button>
+    `
+    toyList.append(cardDiv)
+  }
+
 })
 
